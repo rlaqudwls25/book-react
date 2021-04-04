4d5f8891929aec8b184d76056fcbd621546bec87
rlaqudwls25 <rlaqudwls157@gmail.com>
2021-04-04 20:31:36 +0900

    Modify : 이전값 과 스토어에서 가져오는 state값과 비교해서 렌더링 하기

--- 6-chapter/3-redux-test-react-without-react-redux/src/friend/container/FriendMain.js
+++ 6-chapter/3-redux-test-react-without-react-redux/src/friend/container/FriendMain.js
@@ -1,60 +1,38 @@
-import React from 'react';
-import store from '../../common/store';
-import { getNextFriend } from '../../common/mockData';
-import { addFriend } from '../state';
-import FriendList from '../component/FriendList';
+import React, { useEffect, useReducer } from "react";
+import store from "../../common/store";
+import { getNextFriend } from "../../common/mockData";
+import { addFriend } from "../state";
+import FriendList from "../component/FriendList";
 
-// 리팩터링 후
-class FriendMain extends React.PureComponent {
-  state = {
-    friends: store.getState().friend.friends,
-  };
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() =>
-      this.setState({ friends: store.getState().friend.friends }),
-    );
-  }
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-  onAdd = () => {
+const FriendMain = () => {
+  // useReducer 상태값 변경 함수를 호출 할 때 마다 상태값을 변경
+  const [, forceUpdate] = useReducer((v) => v + 1, 0);
+
+  useEffect(() => {
+    let prevFriends = store.getState().friend.friends;
+    const unsubscribe = store.subscribe(() => {
+      const friends = store.getState().friend.friends;
+      if (prevFriends !== friends) {
+        forceUpdate();
+      }
+      prevFriends = friends;
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const onAdd = () => {
     const friend = getNextFriend();
     store.dispatch(addFriend(friend));
   };
-  render() {
-    console.log('FriendMain render');
-    const friends = store.getState().friend.friends;
-    return (
-      <div>
-        <button onClick={this.onAdd}>친구 추가</button>
-        <FriendList friends={friends} />
-      </div>
-    );
-  }
-}
 
-// 리팩터링 전
-// class FriendMain extends React.Component {
-//   componentDidMount() {
-//     this.unsubscribe = store.subscribe(() => this.forceUpdate());
-//   }
-//   componentWillUnmount() {
-//     this.unsubscribe();
-//   }
-//   onAdd = () => {
-//     const friend = getNextFriend();
-//     store.dispatch(addFriend(friend));
-//   };
-//   render() {
-//     console.log('FriendMain render');
-//     const friends = store.getState().friend.friends;
-//     return (
-//       <div>
-//         <button onClick={this.onAdd}>친구 추가</button>
-//         <FriendList friends={friends} />
-//       </div>
-//     );
-//   }
-// }
+  console.log("FriendMain render");
+  let friends = store.getState().friend.friends;
+  return (
+    <div>
+      <button onClick={onAdd}>친구 추가</button>
+      <FriendList friends={friends} />
+    </div>
+  );
+};
 
 export default FriendMain;
--- 6-chapter/3-redux-test-react-without-react-redux/src/timeline/container/TimelineMain.js
+++ 6-chapter/3-redux-test-react-without-react-redux/src/timeline/container/TimelineMain.js
@@ -1,60 +1,38 @@
-import React from 'react';
-import store from '../../common/store';
-import { getNextTimeline } from '../../common/mockData';
-import { addTimeline } from '../state';
-import TimelineList from '../component/TimelineList.js';
+import React, { useEffect, useReducer } from "react";
+import store from "../../common/store";
+import { getNextTimeline } from "../../common/mockData";
+import { addTimeline } from "../state";
+import TimelineList from "../component/TimelineList.js";
 
 // 리팩터링 후
-class TimelineMain extends React.PureComponent {
-  state = {
-    timelines: store.getState().timeline.timelines,
-  };
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() =>
-      this.setState({ timelines: store.getState().timeline.timelines }),
-    );
-  }
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-  onAdd = () => {
+
+const TimelineMain = () => {
+  const [, forceUpdate] = useReducer((v) => v + 1, 0);
+
+  useEffect(() => {
+    let prevTimeline = store.getState().timeline.timelines;
+    const unsubscribe = store.subscribe(() => {
+      const friends = store.getState().timeline.timelines;
+      if (prevTimeline !== friends) {
+        forceUpdate();
+      }
+      prevTimeline = timelines;
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const onAdd = () => {
     const timeline = getNextTimeline();
     store.dispatch(addTimeline(timeline));
   };
-  render() {
-    console.log('TimelineMain render');
-    const timelines = store.getState().timeline.timelines;
-    return (
-      <div>
-        <button onClick={this.onAdd}>타임라인 추가</button>
-        <TimelineList timelines={timelines} />
-      </div>
-    );
-  }
-}
-
-// 리팩터링 전
-// class TimelineMain extends React.Component {
-//   componentDidMount() {
-//     this.unsubscribe = store.subscribe(() => this.forceUpdate());
-//   }
-//   componentWillUnmount() {
-//     this.unsubscribe();
-//   }
-//   onAdd = () => {
-//     const timeline = getNextTimeline();
-//     store.dispatch(addTimeline(timeline));
-//   };
-//   render() {
-//     console.log('TimelineMain render');
-//     const timelines = store.getState().timeline.timelines;
-//     return (
-//       <div>
-//         <button onClick={this.onAdd}>타임라인 추가</button>
-//         <TimelineList timelines={timelines} />
-//       </div>
-//     );
-//   }
-// }
+  const timelines = store.getState().timeline.timelines;
+  console.log("Timeline render");
+  return (
+    <div>
+      <button onClick={onAdd}>타임라인 추가</button>
+      <TimelineList timelines={timelines} />
+    </div>
+  );
+};
 
 export default TimelineMain;
